8b0cd732b88f53292de6bc60a2b39d1f3c9d6e07
Faixan6190 <faixan6190@gmail.com>
2024-06-16 20:49:14 +0500

    add image

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,18 @@
+import LOGO from "../../assets/images/logo.svg";
+
 function HomePage() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="container">
+      <div className="header-bar">
+        <div>
+          <img src={LOGO} alt="" />
+        </div>
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
