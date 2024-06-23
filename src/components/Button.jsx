caed0b720863f9fe0ec17d51efa1c398cbc7c0fc
Faixan6190 <faixan6190@gmail.com>
2024-06-23 20:44:09 +0500

    add button

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ThemeButton = ({ title }) => {
+  return <button>{title}</button>;
+};
+
+export default ThemeButton;
--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./index.scss";
 
 const TextArea = () => {
-  return <textarea placeholder="Type something..." className="text-area"></textarea>;
+  const textAreaRef = useRef();
+  const resizeTextArea = (event) => {
+    textAreaRef.current.style.height = "24px";
+    textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 12 + "px";
+  };
+  return <textarea ref={textAreaRef} onInput={resizeTextArea} placeholder="Type something..." className="text-area"></textarea>;
 };
 
 export default TextArea;
