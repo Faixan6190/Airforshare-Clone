398e4763216d89decb4e54e3df4ba5e0f8fe11d7
Faizan Ahmed <140724177+Faixan6190@users.noreply.github.com>
2025-01-27 15:28:01 +0500

    change

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -2,7 +2,7 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css"; //Example style, you can use another
+import "prismjs/themes/prism.css";
 import { useState } from "react";
 
 function CodeEditor() {
